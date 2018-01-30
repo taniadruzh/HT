6ef88f64708a58cede5139d8e48270c2788267f5
remdp <remmdp@GMAIL.COM>
2018-01-30 20:08:47 +0200

    14

---
+++ src/ht8_26_01_18/t14/Solution.java
@@ -0,0 +1,64 @@
+package ht8_26_01_18.t14;
+
+import java.io.BufferedReader;
+
+/* Building и School
+1. Расставь правильно наследование между Building(здание) и School(здание школы).
+2. Подумай, объект какого класса должны возвращать методы getSchool и getBuilding.
+3. Измени null на объект класса Building или School.
+*/
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        Building school = getSchool();
+        Building shop = getBuilding();
+        System.out.println(school);
+        System.out.println(shop);
+    }
+    public static Building getSchool()
+    {
+        Building b = new Building();
+        return b;
+    }
+    public static Building getBuilding()
+    {
+        Building b = new Building();
+        return b;
+    }
+    static class School extends Building /*Add your code here*/
+    {
+        private int number;
+
+        public School() {
+        }
+
+        public School(String adres, int number) {
+            super(adres);
+            this.number = number;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "School";
+        }
+    }
+    static class Building /*Add your code here*/
+    {
+        private String adres;
+
+        public Building() {
+        }
+
+        public Building(String adres) {
+            this.adres = adres;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "Building";
+        }
+    }
+}
