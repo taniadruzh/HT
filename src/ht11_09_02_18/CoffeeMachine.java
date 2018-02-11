3f03e2cfa5c8aea835156f73b844385e0362eb16
taniadruzh <tania.druzhchenko@gmail.com>
2018-02-11 19:30:48 +0200

    ht11

--- src/ht11_09_02_18/CoffeeMachine.java
+++ src/ht11_09_02_18/CoffeeMachine.java
@@ -5,7 +5,10 @@ import ht11_09_02_18.Drinks.Drink;
 
 public class CoffeeMachine {
     public static void main(String[] args) {
-        Drink drink1 = new BlackCoffee(1,2,2);
+        int waterCount = 1;
+        int coffeeCount = 1;
+        int sugarCount = 1;
+        Drink drink1 = new BlackCoffee(waterCount,coffeeCount,sugarCount);
         System.out.println(drink1.toString() + " price: " + drink1.getPrice());
 
     }
